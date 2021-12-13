b97af784dda6d033845e4f64f3f8f49731ac75f1
Tannely <taniakorchaga@gmail.com>
2021-12-13 12:27:57 +0200

    multiple inputs

---
+++ input.py
@@ -0,0 +1,8 @@
+a = int(input('Enter a: '))
+b = int(input('Enter b: '))
+c = int(input('Enter c: '))
+print(a, b, c)
+print('--------------------')
+values = input('enter space separated values: ').split()
+a, b, c = map(int, values)
+print(a, b, c,)
